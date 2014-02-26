d0fa1cd4462bf04197ac9af30a785c3a89fb640e
donaldevine <donaldevine@gmail.com>
2014-02-26 20:32:16 +0000

    fixed tests

--- js/converter.js
+++ js/converter.js
@@ -1,3 +1,22 @@
 var Converter = (function () {
 	
-});
+	var converterValue = 2.2;
+
+	return{
+		convertFromImperialToMetric: function(weight) {
+	        var num = parseInt(weight);
+	        if (isNaN(num) == true) {
+	        	throw new Error("Not a number");
+	        }
+	        return Math.round(num / converterValue);
+	    },
+	    convertFromMetricToImperial: function(weight) {
+	        var num = parseInt(weight);
+	        if (isNaN(num) == true) {
+	        	throw new Error("Not a number");
+	        }
+	        return Math.round(num * converterValue);
+	    }
+	}
+	
+}());
